dd881449f35948b7161929a3c3041f659ba78f7e
Oscar Cai <chaehorim@gmail.com>
2019-03-27 13:56:01 +1100

    log merger.sh

---
+++ script/dataAnalyzer/js/init.js
@@ -0,0 +1,71 @@
+
+	var chart ;
+
+    $( document ).ready(function() {
+
+
+    chart = Highcharts.chart('container', {
+
+    title: {
+        text: 'Chart.update'
+    },
+time: {
+        timezone: 'Australia/Sydney'
+    },
+    subtitle: {
+        text: 'Plain'
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+     xAxis: {
+		        type: 'datetime',
+    },
+    yAxis: {
+        title: {
+            text: 'Rainfall (mm)'
+        }
+    },
+    series: [{
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }, {
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }, {
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }, {
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }, {
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }, {
+        type: 'column',
+        // colorByPoint: true,
+        data: [],
+        showInLegend: true
+    }]
+
+	});
+	runChart();
+
+	$('#timeSelector').change(function() {
+		runChart();
+	});
+
+
+});
