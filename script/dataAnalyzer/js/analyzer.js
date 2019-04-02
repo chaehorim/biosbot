28a35fa2d88317e3bea70401ffe2302468e20d12
Oscar Cai <chaehorim@gmail.com>
2019-04-02 13:49:01 +1100

    log analyzer
    log analyzer

--- script/dataAnalyzer/js/analyzer.js
+++ script/dataAnalyzer/js/analyzer.js
@@ -1,90 +1,23 @@
-		function getInitTime(firstDatas) {
-			var minTime = 2009529200
-			for (var i = 0, size = firstDatas.length; i < size; i ++) {
-				if (firstDatas[i][0] < minTime) 
-					minTime = firstDatas[0][i];
-			}
-			return minTime;
+	// function reading and transform data
+	function transformData() {
+		console.log("old" + datax);
+	//"1540738804:205.23:5.86315:205.2:8.03946;1540738804:205.23:5.86315:205.2:8.03946;"
+		var statDatas = [];
+		var tmpSet = datax.split(";");
+
+		for (idx in tmpSet) {
+			if (tmpSet[idx] == '') 
+				continue;
+			console.log("set :" + tmpSet[idx]);
+			var tmpDatas = tmpSet[idx].split(":");
+			var statData = [tmpDatas[0], ((Number(tmpDatas[1]) + Number(tmpDatas[3])) / 2).toFixed(2)];
+			statDatas.push(statData);
+			console.log(statDatas);
 		}
+	}
 
-		function getLastTime(lastDatas){
-			var maxTime = 0;
-			for (var i = 0, size = lastDatas.length; i < size; i ++) {
-				var lastDate = lastDatas[i][lastDatas[i].length -1];
-				if (lastDate > maxTime)
-					maxTime = lastDate;
-			}
-	
-			// return maxTime + 7210;
-			return maxTime ;
-		}
-		function calc(input,initTime, finalTime) {
-			//var initTime = 1539529200;
-			
-			//var increaseTime = 0.2 * 3600;
-			
-			var increaseTime = parseInt($("#timeSelector option:selected").val());
+	// function arrange data by user input
 
-			var tmpCnt = 0;
-			var data = [];
 
-			for (var i = initTime; i <= finalTime; i = i + increaseTime) {
-				var eachCnt = 0;
-				for (var j = 0, size = input.length; j < size; j ++) {
-					if (input[j] < i) {
-						eachCnt ++;					
-					}
-				}
-				var timeCnt = eachCnt - tmpCnt;
-				data.push([(i + 3600 * 11) * 1000, timeCnt]);
-				tmpCnt = eachCnt;				
-			}
+	// function draw chart
 
-			return data;
-		}
-
-		function runChart() {
-			var initTime = getInitTime([riseSuccess, riseFail, downSuccess,downFail]);
-			var finalTime = getLastTime([riseSuccess, riseFail, downSuccess,downFail]);
-
-			var riseSuccData = calc(riseSuccess,initTime, finalTime);
-			var downSuccData = calc(downSuccess,initTime, finalTime);
-			var riseFailData = calc(riseFail,initTime, finalTime);
-			var downFailData = calc(downFail,initTime, finalTime);
-	
-
-			var succData = calc(riseSuccess.concat(downSuccess),initTime, finalTime);
-			var failData = calc(riseFail.concat(downFail),initTime, finalTime);
-			console.log(riseSuccData);
-			console.log(downSuccData);
-			chart.series[0].update({	    
-				name:"rise success",
-				color: "blue",
-			    data: riseSuccData
-			}, true);
-			chart.series[1].update({	    
-				name:"down success",
-				color: "green",
-			    data: downSuccData
-			}, true);
-			chart.series[2].update({	    
-				name:"rise fail",
-				color: "red",
-			    data: riseFailData
-			}, true);
-			chart.series[3].update({	    
-				name:"down fail",
-				color: "pink",
-			    data: downFailData
-			}, true);
-			chart.series[4].update({	    
-				name:"total success",
-				color: "green",
-			    data: succData
-			}, true);
-			chart.series[5].update({	    
-				name:"total failed",
-				color: "orange",
-			    data: failData
-			}, true);
-		}
--- script/dataAnalyzer/log/merger.sh
+++ script/dataAnalyzer/log/merger.sh
@@ -1,2 +1,13 @@
-files=$(ls *.dat)
-echo $files
+> log.js
+
+fileStr=$(ls -m *.dat)
+echo $fileStr
+
+array=($(echo "$fileStr" | tr ',' '\n'))
+
+
+for element in "${array[@]}"
+do
+    echo "$element"
+    cat "$element" >> log.js
+done
