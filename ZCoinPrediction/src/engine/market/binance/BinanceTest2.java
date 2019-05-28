3e6728c62cf00e17d32df9744f65e5a72e8cdbb6
Oscar Cai <chaehorim@gmail.com>
2019-05-28 13:29:15 +1000

    dd

--- ZCoinPrediction/src/engine/market/binance/BinanceTest2.java
+++ ZCoinPrediction/src/engine/market/binance/BinanceTest2.java
@@ -12,7 +12,7 @@ public class BinanceTest2 {
 
 		String coinType = "ETHBTC";
 		Binance bin = new Binance();
-
+	
 		SimpleDateFormat time_formatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
 		System.out.println(bin.getServerTime() - System.currentTimeMillis());
 		System.out.println(time_formatter.format(bin.getServerTime()));
@@ -21,7 +21,7 @@ public class BinanceTest2 {
 //			System.out.println(bin.marketBuy(0.2, 0.030519, coinType));
 //			System.out.println(bin.marketSell(0.2, 88.0, coinType));
 //			OrderDTO order = bin.buyOrder(0.01, 8500.0, coinType);
-			OrderDTO order = bin.sellOrder(1, 0.03119, coinType);
+//			OrderDTO order = bin.sellOrder(1, 0.03119, coinType);
 //			OrderDTO order = new OrderDTO();
 //			order.setOrderId("149870529");
 //			order.setCoinType(coinType);
